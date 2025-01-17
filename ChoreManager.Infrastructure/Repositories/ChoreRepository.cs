54b69d274d81dba7ab52bcff5cf2325389d0e6f6
rafaelMartinez_ <rmartinez@visiotechsecurity.com>
2025-01-17 09:43:52 +0100

    [UPDATE] Se añade el caso de uso de actualizar
    Co-authored-by: Guillermo López Pérez <guiller.lopezz30@gmail.com>

--- ChoreManager.Infrastructure/Repositories/ChoreRepository.cs
+++ ChoreManager.Infrastructure/Repositories/ChoreRepository.cs
@@ -62,7 +62,38 @@ namespace ChoreManager.Infrastructure.Repositories
             }
         }
 
-        public async Task<List<Chore>> GetAllAsync()
+        public async Task UpdateAsync(Chore chore)
+        {
+            try
+            {
+                var choreToUpdate = await _dbContext.Chores.FirstOrDefaultAsync(c => c.Id == chore.Id);
+
+                if (choreToUpdate == null)
+                {
+                    throw new ArgumentException($"No se encontró ninguna tarea con el ID {chore.Id}");
+                }
+
+                choreToUpdate.Title = chore.Title;
+                choreToUpdate.Description = chore.Description;
+                choreToUpdate.DueDate = chore.DueDate;
+                choreToUpdate.Status = chore.Status;
+                choreToUpdate.AssignedUserId = chore.AssignedUserId;
+
+                _dbContext.Chores.Update(choreToUpdate);
+
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new InvalidOperationException("Error al intentar eliminar la tarea", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Ocurrió un error inesperado al eliminar la tarea", ex);
+            }
+        }
+
+        public async Task<IEnumerable<Chore>> GetAllAsync()
         {
             return await _dbContext.Chores
                 .Select(c => new Chore(c.Id, c.Title, c.Description, c.DueDate, c.Status, c.AssignedUserId))
