8768232a0c33e6085763d950efde8cd397dbe777
rafaelMartinez_ <rmartinez@visiotechsecurity.com>
2025-01-17 09:46:28 +0100

    [ADD] Se añade la clase de respuesta de la api
    Co-authored-by: Guillermo López Pérez <guiller.lopezz30@gmail.com>

---
+++ ChoreManager.Api/Common/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ChoreManager.Api.Common
+{
+    public class ApiResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+    }
+}
