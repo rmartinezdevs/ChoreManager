f8e35714ca7bcd03ddee1041badef33fdc56a3cb
rafaelMartinez_ <rmartinez@visiotechsecurity.com>
2025-01-20 08:57:28 +0100

    [ADD] Se crea la clase ErrorResponse
    Co-authored-by: Guillermo López Pérez <guiller.lopezz30@gmail.com>

---
+++ ChoreManager.Api/Common/ErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ChoreManager.Api.Common
+{
+    public class ErrorResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public object? Details { get; set; }
+    }
+}
